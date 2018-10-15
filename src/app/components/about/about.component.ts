bc39a0666dd2582b846d4e62fbf9aa0559907f83
Alex Rodríguez <arodriguez2407@gmail.com>
2018-10-15 18:05:01 -0500

    Maquetado de About listo

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+  public subtitle: string;
+  public email: string;
 
+  constructor() { 
+    this.title = "Alex Rodriguez",
+    this.subtitle = "Asistente de Innovación";
+    this.email = "arodriguez@dechini.com.pe"
+  }
+  
   ngOnInit() {
   }
 
